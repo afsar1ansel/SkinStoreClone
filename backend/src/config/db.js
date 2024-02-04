72be7e95953b3a5889ec5f07d00b654be36aa88b
md afsar <mdafsaransal@gmail.com>
2024-02-04 13:44:25 +0530

    verificationMail

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 async function connectDB() {
     try {
-        
         await mongoose.connect(process.env.MONGO_URL);
         console.log("Database connected successfully")
     } catch (error) {
